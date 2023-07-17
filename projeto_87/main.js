ac5b8cec5951ec20fa7e91b45a05273e7c5beb9f
rafad46 <130005055+rafad46@users.noreply.github.com>
2023-07-17 19:30:44 -0300

    Add files via upload

---
+++ projeto_87/main.js
@@ -0,0 +1,36 @@
+canvas = document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+
+/*
+    Use a função beginPath() para começar um caminho.
+    Então, use strokeStyle para ajustar a cor para "grey".
+    Use lineWidth e ajusta a width para 1.
+    Use rect() para criar um retângulo em 150 e 143 com 430 width e 200 height.
+*/
+
+ctx.beginPath();
+ctx.strokeStyle = "grey";
+ctx.lineWidth = 1;
+ctx.rect(x = 150, y = 143, width= 430, height = 200);
+ctx.stroke();
+
+/*
+    Use a função beginPath() para começar um caminho.
+    Então, use strokeStyle para ajustar a cor para "blue".
+    Use lineWidth e ajuste a width para 5.
+    Use arc() para criar um círculo em  x = 150 e y = 143
+with 40 como radius, 0 startAngle e 2 * Math.PI endAngle.
+*/
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth() = 5;
+ctx.arc(x = 150, y = 143, r = 40, startAngle = 0, endAngle = 2);
+
+// De modo parecido, crie um círculo preto com posição 350 e 210
+
+// De modo parecido, crie um círculo vermelho com posição 210 e 40
+
+// De modo parecido, crie um círculo laranja com posição  300 e 250
+
+// De modo parecido, crie um círculo verde com 
